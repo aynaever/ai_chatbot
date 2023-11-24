599ee65e30f55c6f8c52e482d1a4cbcce22fcb34
Aymane <aymane.naouadir18@gmail.com>
2023-11-24 17:01:02 +0100

    ai chat bot first commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -18,3 +18,20 @@ const config: Config = {
   plugins: [],
 }
 export default config
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['var(--font-inter)'],
+      },
+    },
+  },
+  plugins: [],
+}
